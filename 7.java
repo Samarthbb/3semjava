e864ce8bfbe5988ace4b807b51c1808ce2510eb1
Samarthbb <gaganbeleamgi@gmail.com>
2025-12-23 20:38:57 +0530

    Add files via upload

---
+++ 7.java
@@ -0,0 +1,45 @@
+//a
+importjava.lang.*;
+class Error2
+{
+public static void main(String args[])
+{
+int a=10;
+int b=5;
+int c=5;
+intx,y;
+try
+{
+ x=a/(b-c);
+}
+catch (ArithmeticException e)
+{
+System.out.println("division by zero");
+}
+ y=a/(b+c);
+System.out.println("y=" +y);
+}
+}
+
+
+//b
+class BreakandContinue
+{
+public static void main(String args[])
+    {
+System.out.println("Break Statement\n....................");
+
+for(inti=1;i<=5;i++)
+        {
+if(i==4) break;
+System.out.println(i);
+        }
+System.out.println("Continue Statement\n....................");
+
+for(inti=1;i<=5;i++)
+        {
+if(i==1) continue;
+System.out.println(i);
+        }	
+    }
+}
---
+++ 9.java
@@ -0,0 +1,81 @@
+classFrst implements Runnable
+{
+Thread t;
+Frst()
+{
+t=new Thread(this);
+System.out.println("Good Morning");
+t.start();
+}
+public void run()
+{
+for(inti=0;i<10;i++)
+{
+System.out.println("Good Morning"+i);
+try{
+t.sleep(1000);
+}
+catch(Exception e)
+{
+System.out.println(e);
+}
+}
+}}
+class sec implements Runnable
+{
+Thread t;
+sec()
+{
+t=new Thread(this);
+System.out.println("hello");
+t.start();
+}
+public void run()
+{
+for(inti=0;i<10;i++)
+{
+System.out.println("hello"+i);
+try{
+t.sleep(2000);
+}
+catch(Exception e)
+{
+System.out.println(e);
+}
+}
+}
+}
+class third implements Runnable
+{
+Thread t;
+third()
+{
+t=new Thread(this);
+System.out.println("welcome");
+t.start();
+}
+public void run()
+{
+for(inti=0;i<10;i++)
+{
+System.out.println("welcome"+i);
+try{
+t.sleep(3000);
+}
+catch(Exception e)
+{
+System.out.println(e);
+}
+}
+}
+}
+public class Multithread
+{
+public static void main(String arg[])
+{
+newFrst();
+new sec();
+new third();
+}
+}
+
